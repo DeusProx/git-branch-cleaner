47b23a9366fda6603c49fe425bac67733eefa386
Gordon Lawrenz <gordon@lawrenz.email>
2023-10-21 16:52:10 +0200

    Build simple TUI where you can leave when pressing q

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,45 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::{stderr, Result};
+
+use crossterm::ExecutableCommand;
+use crossterm::event::{self, KeyCode, KeyEventKind};
+use crossterm::terminal::{enable_raw_mode, disable_raw_mode, EnterAlternateScreen};
+use ratatui::Terminal;
+use ratatui::prelude::CrosstermBackend;
+use ratatui::widgets::Paragraph;
+
+fn main() -> Result<()> {
+
+    stderr().execute(EnterAlternateScreen)?;
+    enable_raw_mode()?;
+
+
+    let mut terminal = Terminal::new(CrosstermBackend::new(stderr()))?;
+    terminal.clear()?;
+
+    loop {
+        terminal.draw(|frame| {
+            let area = frame.size();
+            frame.render_widget(
+                Paragraph::new("Welcome to Git Branch Cleaner! (Leave by pressing q)"),
+                area
+            )
+        })?;
+
+        if event::poll(std::time::Duration::from_millis(100))? {
+            if let event::Event::Key(event::KeyEvent {
+                kind: KeyEventKind::Press,
+                code: KeyCode::Char(char),
+                ..
+            }) = event::read()? {
+                match char {
+                    'q' => break,
+                    _ => {}
+                }
+            }
+        }
+    }
+
+    disable_raw_mode()?;
+    Ok(())
 }
+
